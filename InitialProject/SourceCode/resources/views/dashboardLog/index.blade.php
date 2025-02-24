8fa02dce5832f0bb011e0c1b71c1d06495f54dca
Paweesuda-Thippayanasa <paweesuda.t@kkumail.com>
2025-02-25 00:44:20 +0700

    update: [card] As an admin, I want a dashboard..., [detail] update ui dashboard log icon card

--- InitialProject/SourceCode/resources/views/dashboardLog/index.blade.php
+++ InitialProject/SourceCode/resources/views/dashboardLog/index.blade.php
@@ -72,7 +72,7 @@
                     <div class="card h-100 border-0 shadow-sm hover-card rounded-2">
                         <div class="card-body">
                             <div class="d-flex align-items-center mb-3">
-                                <div class="icon-wrapper d-flex align-items-center  justify-content-between bg-primary-soft rounded-circle me-3">
+                                <div class="icon-wrapper d-flex align-items-center  justify-content-between bg-info-soft rounded-circle me-3">
                                     <i class="fas fa-users text-primary fa-2x"></i>
                                 </div>
                                 <h6 class="card-title text-muted mb-0">จำนวนผู้ใช้ทั้งหมด</h6>
@@ -90,7 +90,7 @@
                     <div class="card h-100 border-0 shadow-sm hover-card rounded-2">
                         <div class="card-body">
                             <div class="d-flex align-items-center  mb-3">
-                                <div class="icon-wrapper  bg-success-soft rounded-circle me-3">
+                                <div class="icon-wrapper d-flex align-items-center  justify-content-between bg-info-soft rounded-circle me-3">
                                     <i class="fas fa-book text-success fa-2x"></i>
                                 </div>
                                 <h6 class="card-title text-muted mb-0">จำนวนงานวิจัยทั้งหมด</h6>
@@ -131,7 +131,7 @@
                         <div class="card-body">
                             <div class="d-flex align-items-center mb-3">
                                 <div
-                                    class="icon-wrapper bg-warning-soft rounded-circle d-flex align-items-center justify-content-center p-3">
+                                    class="icon-wrapper d-flex align-items-center  justify-content-between bg-info-soft rounded-circle me-3">
                                     <i class="fas fa-code text-warning fa-2x"></i>
                                 </div>
                                 <h6 class="card-title text-muted mb-0">จำนวนการเรียก API ในวันนี้</h6>
