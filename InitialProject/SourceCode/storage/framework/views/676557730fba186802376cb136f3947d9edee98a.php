4c9b0c579e7f512b1c41fac75f53c49d7945f844
Chanin-Thunsiripattanatada <chanin.t@kkumail.com>
2025-03-11 00:09:50 +0700

    Update: [card] As an admin, I want a report of user's activities..., [detail] improve data export functionality and enhance report layout

--- InitialProject/SourceCode/storage/framework/views/676557730fba186802376cb136f3947d9edee98a.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('title', __('Not Found')); ?>
-<?php $__env->startSection('code', '404'); ?>
-<?php $__env->startSection('message', __('Not Found')); ?>
-
-<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH E:\year3_2\softwareEng\soften_jectgroup\git-group-repository-group-2-sec-1\InitialProject\SourceCode\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
